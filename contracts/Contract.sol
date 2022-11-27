57485b23b1288c2af0bb57549e7ecb890093011e
moikapy <warrenckhan@gmail.com>
2022-11-27 16:01:08 -0500

    add permissions to contract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC20Base.sol";
+import "@thirdweb-dev/contracts/extension/Permissions.sol";
 
-contract Contract is ERC20Base {
+contract Contract is ERC20Base, Permissions {
       constructor(
         string memory _name,
         string memory _symbol
