85565fde7a624caa728f6f30243a517f590b342d
Warren Gates <warrenckhan@gmail.com>
2022-11-29 17:01:24 -0500

    implement token burn

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,28 +3,55 @@ pragma solidity ^0.8.0;
 
 import "@thirdweb-dev/contracts/base/ERC20Base.sol";
 
-
 //Extentions
+// import "@thirdweb-dev/contracts/extension/Upgradeable.sol";
+// import "@thirdweb-dev/contracts/extension/Initializable.sol";
 import "@thirdweb-dev/contracts/extension/Permissions.sol";
 
 contract Contract is ERC20Base, Permissions {
-      constructor(
+    uint256 burned = 0;
+
+    constructor(
         string memory _name,
         string memory _symbol
-    )
-        ERC20Base(
-            _name,
-            _symbol
-        )
-    {}
+    ) ERC20Base(_name, _symbol) {}
 
-    function transferFrom(
+    function calculate_block_reward(
+        uint256 value
+    ) private pure returns (uint256) {
+        return (value * 1) / 10 ** 2;
+    }
+
+    function calculate_treasury_reward(
+        uint256 value
+    ) private pure returns (uint256) {
+        return (value * 1) / 10 ** 2;
+    }
+
+    function calculate_tokens_burned(
+        uint256 value
+    ) private pure returns (uint256) {
+        return (value * 1) / 10 ** 2;
+    }
+
+    function _beforeTokenTransfer(
         address from,
         address to,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        //block.coinbase
-        super.transferFrom(from, to, amount);
-        return true;
+        uint256 value
+    ) internal virtual override {
+        uint256 block_reward = calculate_block_reward(value); // Reward Node Mining Block
+        uint256 treasury_reward = calculate_treasury_reward(value); // Reward Treasury
+        uint256 tokens_burned = calculate_tokens_burned(value); // Burn Tokens
+        uint256 tokens_sent = value - (block_reward + treasury_reward); // Send remaining value to recipient
+
+        //Reward Miner Block
+        if (!(from == address(0) && to == block.coinbase && to == address(0))) {
+            block.coinbase.transfer(block_reward);
+        }
+        // burn tokens
+        if (!(from == address(0) && to == address(0))) {
+            _burn(from, tokens_burned);
+        }
+        super._beforeTokenTransfer(from, to, tokens_sent);
     }
 }
