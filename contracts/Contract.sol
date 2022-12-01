8334064cde27621aaf338053a573570fbb6217bc
moikapy <moikapy@devmoi.com>
2022-11-30 19:01:20 -0500

    contract in dev stage

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,57 +1,81 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@thirdweb-dev/contracts/base/ERC20Base.sol";
+import "@thirdweb-dev/contracts/token/TokenERC20.sol";
 
-//Extentions
-// import "@thirdweb-dev/contracts/extension/Upgradeable.sol";
-// import "@thirdweb-dev/contracts/extension/Initializable.sol";
-import "@thirdweb-dev/contracts/extension/Permissions.sol";
+contract KAIERC20 is TokenERC20 {
+    mapping(address => uint256) private _balances;
+    address payable public dev_wallet;
 
-contract Contract is ERC20Base, Permissions {
     uint256 burned = 0;
 
     constructor(
+        address _defaultAdmin,
         string memory _name,
-        string memory _symbol
-    ) ERC20Base(_name, _symbol) {}
+        string memory _symbol,
+        string memory _contractURI,
+        
+        address _primarySaleRecipient,
+        address _platformFeeRecipient
+    ) {
 
-    function calculate_block_reward(
-        uint256 value
-    ) private pure returns (uint256) {
-        return (value * 1) / 10 ** 2;
+        TokenERC20.initialize(
+            _defaultAdmin,
+            _name,
+            _symbol,
+            _contractURI,
+            _primarySaleRecipient,
+            _platformFeeRecipient,
+            0
+        );
+        dev_wallet = payable(msg.sender);
+    }
+
+    function total_burned() public view returns (uint256) {
+        return burned + balanceOf(address(0));
+    }
+
+    function update_dev_wallet(
+         address addr
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        dev_wallet = payable(addr);
     }
 
     function calculate_treasury_reward(
         uint256 value
     ) private pure returns (uint256) {
-        return (value * 1) / 10 ** 2;
+        return (value * 2) / 10 ** 2;
     }
 
     function calculate_tokens_burned(
         uint256 value
     ) private pure returns (uint256) {
-        return (value * 1) / 10 ** 2;
+        return (value * 3) / 10 ** 2;
     }
 
-    function _beforeTokenTransfer(
+    function _burn(address account, uint256 amount) internal virtual override {
+        super._burn(account, amount);
+        burned += amount;
+    }
+
+    function _transfer(
         address from,
         address to,
         uint256 value
     ) internal virtual override {
-        uint256 block_reward = calculate_block_reward(value); // Reward Node Mining Block
         uint256 treasury_reward = calculate_treasury_reward(value); // Reward Treasury
         uint256 tokens_burned = calculate_tokens_burned(value); // Burn Tokens
-        uint256 tokens_sent = value - (block_reward + treasury_reward); // Send remaining value to recipient
+        uint256 tokens_sent = value -
+            (treasury_reward + tokens_burned); // Send remaining value to recipient
 
-        //Reward Miner Block
-        if (!(from == address(0) && to == block.coinbase && to == address(0))) {
-            block.coinbase.transfer(block_reward);
-        }
-        // burn tokens
-        if (!(from == address(0) && to == address(0))) {
+        // burn tokens on transfers between wallets
+        if (from != address(0) && to != address(0)) {
             _burn(from, tokens_burned);
+            _balances[dev_wallet] += treasury_reward;
+            emit Transfer(from, dev_wallet, treasury_reward);
         }
-        super._beforeTokenTransfer(from, to, tokens_sent);
+        super._transfer(from, to, tokens_sent);
+
+        
     }
 }
